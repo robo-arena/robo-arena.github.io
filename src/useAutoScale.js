b9400d3a580d6b9fb0b758f69468a40548656575
pranavatreya <pranavatreya@utexas.edu>
2025-06-19 19:50:10 -0700

    zooms out less

--- src/useAutoScale.js
+++ src/useAutoScale.js
@@ -1,6 +1,6 @@
 import { useLayoutEffect } from 'react';
 
-export default function useAutoScale(designWidth = 1200) {
+export default function useAutoScale(designWidth = 900) {
   useLayoutEffect(() => {
     function setScale() {
       const scale = Math.min(1, window.innerWidth / designWidth);
